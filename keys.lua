fa51d67fb0ece82ac79c2708354343931aa43044
ImReallyastra <astrinhaforever1@gmail.com>
2025-08-31 23:28:44 -0300

    Deletando key SUPA-PD6M5I1BRF-9GDNZST2GL-NW7RKRL3R9

--- keys.lua
+++ keys.lua
@@ -1,12 +1,6 @@
 return {
   {
     Identificador = "Astra",
-    Hwid = "Empty",
-    Key = "SUPA-PD6M5I1BRF-9GDNZST2GL-NW7RKRL3R9",
-    Expira = 10396607271,
-  },
-  {
-    Identificador = "Astra",
     Hwid = "B8576E3F-1BA0-451F-AD02-93DC41B29743",
     Key = "SUPA-10DU6BCHCT-ACVA1GVZA8-3NH8L7GEFS",
     Expira = 88156607287,
