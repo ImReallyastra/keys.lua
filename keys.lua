22ac30d1613cf187bca7b4525a8c28f926ba3d24
ImReallyastra <astrinhaforever1@gmail.com>
2025-08-31 23:28:09 -0300

    Adicionando key SUPA-10DU6BCHCT-ACVA1GVZA8-3NH8L7GEFS

--- keys.lua
+++ keys.lua
@@ -5,4 +5,10 @@ return {
     Key = "SUPA-PD6M5I1BRF-9GDNZST2GL-NW7RKRL3R9",
     Expira = 10396607271,
   },
+  {
+    Identificador = "Astra",
+    Hwid = "Empty",
+    Key = "SUPA-10DU6BCHCT-ACVA1GVZA8-3NH8L7GEFS",
+    Expira = 88156607287,
+  },
 }
