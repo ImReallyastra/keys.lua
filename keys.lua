0c645b37ad4371be675cf7bdb1b3519e223fa881
ImReallyastra <astrinhaforever1@gmail.com>
2025-09-04 22:08:18 -0300

    Deletando key SUPA-OXRNMZVLR5-EGX5Y1CNIU-0S6MQKBOFT

--- keys.lua
+++ keys.lua
@@ -5,10 +5,4 @@ return {
     Key = "SUPA-10DU6BCHCT-ACVA1GVZA8-3NH8L7GEFS",
     Expira = 88156607287,
   },
-  {
-    Identificador = "Denny",
-    Hwid = "B8576E3F-1BA0-451F-AD02-93DC41B29743",
-    Key = "SUPA-OXRNMZVLR5-EGX5Y1CNIU-0S6MQKBOFT",
-    Expira = 1757638361,
-  },
 }
