88e8a9264a540ca48e0c249c73f394c336355a41
ImReallyastra <astrinhaforever1@gmail.com>
2025-09-05 02:43:54 -0300

    Removendo keys expiradas

--- keys.lua
+++ keys.lua
@@ -5,10 +5,4 @@ return {
     Key = "SUPA-10DU6BCHCT-ACVA1GVZA8-3NH8L7GEFS",
     Expira = 88156607287,
   },
-  {
-    Identificador = "Denny",
-    Hwid = "7AA2E7E4-4451-4426-A621-25AF6FA6DB47",
-    Key = "SUPA-QL791J07XZ-MDKJ0AW35T-A31ERZLD9Q",
-    Expira = 1757048936,
-  },
 }
