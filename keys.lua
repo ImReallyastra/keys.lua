4e280be2636351913b80793b7833c58a941e9b96
ImReallyastra <astrinhaforever1@gmail.com>
2025-09-05 01:08:26 -0300

    Adicionando key SUPA-DR8TWCIY54-BJB1IQCLEI-5X19OV2P2L

--- keys.lua
+++ keys.lua
@@ -5,4 +5,10 @@ return {
     Key = "SUPA-10DU6BCHCT-ACVA1GVZA8-3NH8L7GEFS",
     Expira = 88156607287,
   },
+  {
+    Identificador = "Denny",
+    Hwid = "Empty",
+    Key = "SUPA-DR8TWCIY54-BJB1IQCLEI-5X19OV2P2L",
+    Expira = 1757131705,
+  },
 }
