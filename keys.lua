3e9e8f49435721643818c99d335e03ca022a1a08
ImReallyastra <astrinhaforever1@gmail.com>
2025-09-06 21:31:13 -0300

    Deletando key SUPA-SS8W0HJ3ST-9IT6S5JREI-RPW2GH3HBA

--- keys.lua
+++ keys.lua
@@ -5,10 +5,4 @@ return {
     Key = "SUPA-YTLDW1JKTR-USE117V6CB-Z89UVDTZSG",
     Expira = 88157118659,
   },
-  {
-    Identificador = "Astra",
-    Hwid = "Empty",
-    Key = "SUPA-SS8W0HJ3ST-9IT6S5JREI-RPW2GH3HBA",
-    Expira = 1757291461,
-  },
 }
