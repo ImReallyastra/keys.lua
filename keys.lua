eee2ef26bd70f84a614776d14ee5f4eae5d33288
ImReallyastra <astrinhaforever1@gmail.com>
2025-09-06 21:31:45 -0300

    Registrando HWID B8576E3F-1BA0-451F-AD02-93DC41B29743

--- keys.lua
+++ keys.lua
@@ -1,7 +1,7 @@
 return {
   {
     Identificador = "Astra",
-    Hwid = "Empty",
+    Hwid = "B8576E3F-1BA0-451F-AD02-93DC41B29743",
     Key = "SUPA-YTLDW1JKTR-USE117V6CB-Z89UVDTZSG",
     Expira = 88157118659,
   },
