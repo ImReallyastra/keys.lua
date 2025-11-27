bb51cee9a762818bcc44d67776536676d32c2436
ImReallyastra <astrinhaforever1@gmail.com>
2025-11-27 00:26:28 -0300

    Atualizar o keys.lua

--- keys.lua
+++ keys.lua
@@ -1,7 +1,7 @@
 return {
   {
     Identificador = "Astra",
-    Hwid = "4F9D7CEF-9FB1-4670-AA03-9BCE7D2165D1",
+    Hwid = "03EE07B2-BC68-480B-B5EB-AA2C4CDC006E",
     Key = "SUPA-YTLDW1JKTR-USE117V6CB-Z89UVDTZSG",
     Expira = 88157118659,
   },
